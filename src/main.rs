949b237fd416019d1f818223363538c38c46f970
zhanchihu <zhanchihu@gmail.com>
2024-09-22 08:02:00 +0000

    add uri dispatch

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,25 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buff_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buff_reader.lines()
-    .map(|result| result.unwrap())
-    .take_while(|line| !line.is_empty())
-    .collect();
-    println!("Request: {http_request:#?}");
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
-    
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    
+    let request_line = buff_reader.lines().next().unwrap().unwrap();
+    let status_line: String;
+    let length: usize;
+    let contents: String;
+
+    if request_line == "GET / HTTP/1.1" {
+        status_line = "HTTP/1.1 200 OK".to_string();
+        contents = fs::read_to_string("hello.html").unwrap();
+        length = contents.len();
+    } else {
+        status_line = "HTTP/1.1 404 NOT FOUND".to_string();
+        contents = "not found".to_string();
+        length = contents.len();
+    }
+
+    let response = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
     stream.write_all(response.as_bytes()).unwrap();
 }
 
