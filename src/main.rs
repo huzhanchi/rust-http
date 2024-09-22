6d1bb9adca0f4d671d09494843b088ae205dc63e
zhanchihu <zhanchihu@gmail.com>
2024-09-22 09:14:10 +0000

    refactor

--- src/main.rs
+++ src/main.rs
@@ -19,19 +19,15 @@ fn handle_connection(mut stream: TcpStream) {
     let buff_reader = BufReader::new(&mut stream);
 
     let request_line = buff_reader.lines().next().unwrap().unwrap();
-    let status_line: String;
-    let length: usize;
-    let contents: String;
 
-    if request_line == "GET / HTTP/1.1" {
-        status_line = "HTTP/1.1 200 OK".to_string();
-        contents = fs::read_to_string("hello.html").unwrap();
-        length = contents.len();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        status_line = "HTTP/1.1 404 NOT FOUND".to_string();
-        contents = "not found".to_string();
-        length = contents.len();
-    }
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
     let response = format!(
         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
